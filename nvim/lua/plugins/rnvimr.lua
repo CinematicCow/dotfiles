e35e9df8924b356cf4a1245accde1c236e4144b3
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2026-01-10 12:42:20 +0530

    fix(nvim): rnvimr cmd

--- nvim/lua/plugins/rnvimr.lua
+++ nvim/lua/plugins/rnvimr.lua
@@ -6,7 +6,7 @@ return {
         vim.g.rnvimr_draw_border = 1
         vim.g.rnvimr_hide_gitignore = 1
         vim.g.rnvimr_enable_bw = 1
-        vim.g.rnvimr_ranger_cmd = { 'ranger', '--cmd=set draw_borders both' }
+        vim.g.rnvimr_ranger_cmd = { 'ranger' }
         vim.g.rnvimr_pick_enable = 1
     end
 }
